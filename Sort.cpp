ce38954be5c815a92994ac2837920ef4ec2c4ddf
jj-jc <juanjosejuradocamino@gmail.com>
2022-07-07 16:57:31 +0200

    TypePunning

---
+++ Sort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+int main()
+{
+    std::vector<int> values = {3, 5, 1, 4, 2};
+
+    std::sort(values.begin(), values.end(), std::greater<int>());
+
+    for (int value : values)
+        std::cout << value << std::endl;
+    
+    std::sort(values.begin(), values.end(), [](int a, int b)
+    { // if you use an function you have to return true if a should be before than b and false otherwise.
+        if (a == 1)
+            return false;
+        if (b == 1)
+            return true;
+        return a < b;
+    
+    });
+
+    for (int value : values)
+        std::cout << value << std::endl;
+
+}
---
+++ TypePunning.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+
+struct Entity
+{
+    int x, y;
+};
+
+int main()
+{
+    int a = 50;
+    double value = a; // implicit conversion from int to double
+    double doubleValue = (double)a; // explicit conversion from int to double
+    std::cout << value << std::endl;
+    std::cout << doubleValue << std::endl;
+
+    double valueType = *(double*)&a; // type punning the integer to a double. This take the information literally from the int memory allocation and 
+    // paste it into the double variable allocation. 
+    std::cout << valueType << std::endl;
+
+    Entity e = {5, 8};
+    int* position = (int*)&e;
+    std::cout << position[0] << ", " << position[1] << std::endl;
+
+    int y = *(int*)((char*)&e + 4);
+    std::cout << y << std::endl;
+
+    int y2 = *(int*)((int*)&e + 1);
+    std::cout << y2 << std::endl;
+
+}
