2d99085212d6daaa74ce19ac3115162a26beae11
jj-jc <juanjosejuradocamino@gmail.com>
2022-07-13 14:03:14 +0200

    Added a infinite loop to try the Action and conditional breakpoints

--- MultidimensionalArray.cpp
+++ MultidimensionalArray.cpp
@@ -32,43 +32,45 @@ class Timer
 int main()
 {
     
-    int* array = new int[50]; // allocate 200 bytes of memory (for 50 ints)
-    delete[] array;
-    
-    std::cout << "Multiple arrays: ";
-    {
-        Timer timer;
-        // Creating a multidimensional array with new
-        int** a2d = new int*[50]; // allocate 200 bytes of memoery ( for 50 integerpointers)
-        for (int i = 0; i < 50; i++)
-            a2d[i] = new int[50];
-
-        // accessing to multidimensional array
-        for (int y = 0; y < 50; y++)
+    while(1){
+        int* array = new int[50]; // allocate 200 bytes of memory (for 50 ints)
+        delete[] array;
+        
+        std::cout << "Multiple arrays: ";
         {
-            for (int x = 0; x < 50; x++)
-            {
-                a2d[x][y] = 2;
-            }
-        }
+            Timer timer;
+            // Creating a multidimensional array with new
+            int** a2d = new int*[50]; // allocate 200 bytes of memoery ( for 50 integerpointers)
+            for (int i = 0; i < 50; i++)
+                a2d[i] = new int[50];
 
-        // Deleting a multidimensional array with delete
-        for (int i = 0; i < 50; i++)
-            delete[] a2d[i]; // this is very important to take into account, you have to delete every array of integers.
-        delete[] a2d; // and of course you have to delete the array of pointers.
-    }
-    
-    std::cout << "Multiple elements: ";
-    {
-        Timer time;
-        int* b2d = new int[50 * 50];
-        for (int x = 0; x < 50; x++)
-        {
+            // accessing to multidimensional array
             for (int y = 0; y < 50; y++)
             {
-                b2d[x * 50 + y];
+                for (int x = 0; x < 50; x++)
+                {
+                    a2d[x][y] = x;
+                }
             }
+
+            // Deleting a multidimensional array with delete
+            for (int i = 0; i < 50; i++)
+                delete[] a2d[i]; // this is very important to take into account, you have to delete every array of integers.
+            delete[] a2d; // and of course you have to delete the array of pointers.
         }
-        delete[] b2d;
+        
+        std::cout << "Multiple elements: ";
+        {
+            Timer time;
+            int* b2d = new int[50 * 50];
+            for (int x = 0; x < 50; x++)
+            {
+                for (int y = 0; y < 50; y++)
+                {
+                    b2d[x * 50 + y];
+                }
+            }
+            delete[] b2d;
+        }
     }
 }
