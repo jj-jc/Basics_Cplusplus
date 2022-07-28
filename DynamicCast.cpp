738270db0e46c6a0a224527eb7ecfe87474972e6
jj-jc <juanjosejuradocamino@gmail.com>
2022-07-28 11:45:19 +0200

    Added DynamicCast and Structure_Bindings

---
+++ DynamicCast.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class Entity
+{
+    
+};
+
+class Player : public Entity
+{
+
+};
+
+class Enemy : public Entity
+{
+
+};
+
+int main()
+{
+    Player* player = new Player();
+    Entity* e = player; 
+
+    
+}
