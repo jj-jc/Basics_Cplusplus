e7f07b07eb13d5fb1107ee84405f6257bd56e1b8
jj-jc <juanjosejuradocamino@gmail.com>
2024-06-16 16:12:44 +0200

    Changes in Arrat. Write it properly

--- Arrays.cpp
+++ Arrays.cpp
@@ -4,15 +4,21 @@
 
 void printArray(std::array<int,5> array)
 {
--
+        std::cout << "Array = { ";
+    for (const auto& item: array)
+    {
+        if(array[0] != item)
+            std::cout << ", ";
+        std::cout << item;
+    }
+    std::cout << "}" << std::endl;
 }
 
 int main()
 {
-    std::array<int, 5> data;
-    data[0] = 2;
-    data[4] = 1;
+    std::array<int, 5> data{1,2,3,5,7};
+    
+    printArray(data);
 
-
-
+return true;
 }
